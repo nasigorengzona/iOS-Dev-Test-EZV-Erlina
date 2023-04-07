4915c189ee6de02d637b9562b786aa27c6c6ce25
Erlina Ng <erlina.ng@gmail.com>
2023-04-07 09:04:46 +0700

    Initial Commit

---
+++ iOS-Dev-Test-EZV-Erlina/iOS_Dev_Test_EZV_ErlinaApp.swift
@@ -0,0 +1,20 @@
+//
+//  iOS_Dev_Test_EZV_ErlinaApp.swift
+//  iOS-Dev-Test-EZV-Erlina
+//
+//  Created by erlina ng on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Dev_Test_EZV_ErlinaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
